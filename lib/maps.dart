e1bfb0eac3d433bd3eee504517e4802d81331802
ankurraj845 <ankurraj643@gmail.com>
2022-09-14 16:34:21 +0530

    maps or Dictionary

---
+++ lib/maps.dart
@@ -0,0 +1,13 @@
+import 'dart:io';
+void main() {
+
+  var map_name = {'key1':'ankur', 'key2':'raj' , 'key3': 'prasad' } ;
+  var map_name2 = {'x': '1', 'y': '2', 'z':'3'  };
+
+
+  print(map_name);
+  print(map_name.isNotEmpty);
+  print(map_name.length);
+  print(map_name2);
+
+}
